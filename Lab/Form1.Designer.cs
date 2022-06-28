2ebd584511164ed9b23cce07c46c3864c96b6eb4
Stressica <itsgogolsophia@gmail.com>
2022-06-28 19:41:24 +0300

    Lab1

---
+++ Lab/Form1.Designer.cs
@@ -0,0 +1,239 @@
+﻿
+namespace Lab
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.for_x = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.for_y = new System.Windows.Forms.TextBox();
+            this.for_a = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.Result_1 = new System.Windows.Forms.TextBox();
+            this.Result_2 = new System.Windows.Forms.TextBox();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.DarkBlue;
+            this.button1.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button1.Location = new System.Drawing.Point(62, 335);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(160, 50);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Розрахувати";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.DarkCyan;
+            this.button2.ForeColor = System.Drawing.SystemColors.ButtonFace;
+            this.button2.Location = new System.Drawing.Point(569, 335);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(160, 50);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "Розрахувати";
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(258, 76);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(46, 25);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Х =";
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(305, 394);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(193, 44);
+            this.button3.TabIndex = 3;
+            this.button3.Text = "Вбити форму :)";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.MouseClick += new System.Windows.Forms.MouseEventHandler(this.button3_MouseClick);
+            // 
+            // for_x
+            // 
+            this.for_x.Location = new System.Drawing.Point(326, 64);
+            this.for_x.Multiline = true;
+            this.for_x.Name = "for_x";
+            this.for_x.Size = new System.Drawing.Size(172, 40);
+            this.for_x.TabIndex = 4;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label2.Location = new System.Drawing.Point(258, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(44, 25);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "У =";
+            // 
+            // for_y
+            // 
+            this.for_y.Location = new System.Drawing.Point(326, 110);
+            this.for_y.Multiline = true;
+            this.for_y.Name = "for_y";
+            this.for_y.Size = new System.Drawing.Size(172, 43);
+            this.for_y.TabIndex = 6;
+            // 
+            // for_a
+            // 
+            this.for_a.Location = new System.Drawing.Point(326, 159);
+            this.for_a.Multiline = true;
+            this.for_a.Name = "for_a";
+            this.for_a.Size = new System.Drawing.Size(172, 41);
+            this.for_a.TabIndex = 7;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label3.Location = new System.Drawing.Point(258, 171);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(45, 25);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "А =";
+            // 
+            // Result_1
+            // 
+            this.Result_1.Location = new System.Drawing.Point(62, 286);
+            this.Result_1.Name = "Result_1";
+            this.Result_1.Size = new System.Drawing.Size(160, 22);
+            this.Result_1.TabIndex = 9;
+            // 
+            // Result_2
+            // 
+            this.Result_2.Location = new System.Drawing.Point(569, 286);
+            this.Result_2.Name = "Result_2";
+            this.Result_2.Size = new System.Drawing.Size(160, 22);
+            this.Result_2.TabIndex = 10;
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label4.Location = new System.Drawing.Point(71, 239);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(138, 25);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Результат 1";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, ((System.Drawing.FontStyle)((System.Drawing.FontStyle.Bold | System.Drawing.FontStyle.Italic))), System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label5.Location = new System.Drawing.Point(580, 239);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(138, 25);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Результат 2";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label7.Location = new System.Drawing.Point(615, 408);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(95, 17);
+            this.label7.TabIndex = 14;
+            this.label7.Text = "|^ зелена ^|";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 7.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label6.Location = new System.Drawing.Point(96, 408);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(104, 17);
+            this.label6.TabIndex = 15;
+            this.label6.Text = "|^ червона ^|";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.LightGoldenrodYellow;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.Result_2);
+            this.Controls.Add(this.Result_1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.for_a);
+            this.Controls.Add(this.for_y);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.for_x);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Lab1_byGogol";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.TextBox for_x;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox for_y;
+        private System.Windows.Forms.TextBox for_a;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox Result_1;
+        private System.Windows.Forms.TextBox Result_2;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label6;
+    }
+}
+
---
+++ Lab/Form1.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Lab
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+            switch (rand.Next(0, 10))
+            {
+                case 0: button2.BackColor = Color.Green; break;
+                case 1: button2.BackColor = Color.Red; break;
+                case 2: button2.BackColor = Color.Yellow; break;
+                case 3: button2.BackColor = Color.White; break;
+                case 4: button2.BackColor = Color.Gray; break;
+                case 5: button2.BackColor = Color.Blue; break;
+                case 6: button2.BackColor = Color.Cyan; break;
+                case 7: button2.BackColor = Color.DarkBlue; break;
+                case 8: button2.BackColor = Color.DarkCyan; break;
+                case 9: button2.BackColor = Color.DarkGray; break;
+                case 10: button2.BackColor = Color.DarkRed; break;
+            }
+
+            if (button1.BackColor == Color.Red)
+            { 
+                double x = double.Parse(for_x.Text);
+                double y = double.Parse(for_y.Text);
+
+                if (x!=0 && y!=0)
+                {
+                    double res1 = (5 * x / (Math.Pow(x, 3) + Math.Pow(y, 3)) - 1 / (Math.Tan(3 * x / y)));
+                    Result_1.Text = Convert.ToString(res1);
+                }
+                else
+                {
+                    Result_1.Text = "Error!";
+                }                             
+            }           
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Random rand = new Random();
+
+            switch (rand.Next(0, 10))
+            {
+                case 0: button1.BackColor = Color.Green; break;
+                case 1: button1.BackColor = Color.Red; break;
+                case 2: button1.BackColor = Color.Yellow; break;
+                case 3: button1.BackColor = Color.White; break;
+                case 4: button1.BackColor = Color.Gray; break;
+                case 5: button1.BackColor = Color.Blue; break;
+                case 6: button1.BackColor = Color.Cyan; break;
+                case 7: button1.BackColor = Color.DarkBlue; break;
+                case 8: button1.BackColor = Color.DarkCyan; break;
+                case 9: button1.BackColor = Color.DarkGray; break;
+                case 10: button1.BackColor = Color.DarkRed; break;
+                    
+            }
+            if (button2.BackColor == Color.Green)
+            {
+                double x = int.Parse(for_x.Text);
+                double a = int.Parse(for_a.Text);
+
+                if ((a != 0) && (x > 0 && a > 0) || (x < 0 && a < 0))
+                {
+                    double res2 = Math.Sign(x-2*a)+Math.Log((x/a),3);
+                    Result_2.Text = Convert.ToString(res2);
+                }
+                else
+                {
+                    Result_2.Text = "Error!";
+                }
+            }           
+        }
+
+        private void button3_MouseClick(object sender, MouseEventArgs e)
+        {
+            MessageBox.Show(
+                "Прости. Прощай. Привет", 
+                "Важливе повідомлення");
+            this.Close();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
